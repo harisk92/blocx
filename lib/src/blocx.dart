975520426b9dc1b6acbbc5463c8d6fd02e89a8bf
harisk92 <programmerguy92@gmail.com>
2018-12-13 11:03:59 +0100

    Documented API

--- lib/src/blocx.dart
+++ lib/src/blocx.dart
@@ -8,29 +8,54 @@ abstract class Bloc<A, S> {
   PublishSubject<A> _artifactSubject = PublishSubject<A>();
   BehaviorSubject<S> _stateSubject = BehaviorSubject<S>();
 
+  /*
+  Stream that exposes latest state
+   */
   Stream<S> get state => _stateSubject.stream;
 
+  /*
+  Stream that exposes artifacts.Artifacts are meant to be side effects that come with state update for example deleting
+  of entry requires you to show dialog or toast message and this could be achieved with artifact.
+   */
   Stream<A> get artifact => _artifactSubject.stream;
 
   Bloc() {
     _listen();
   }
 
+  /*
+  Needed to be called when BloC is no more needed
+   */
   dispose() {
     _actionSubject.close();
     _stateSubject.close();
     _artifactSubject.close();
   }
 
+  /*
+  Used for dispatching actions
+   */
   void dispatch(A action) {
     if (!_actionSubject.isClosed && !_actionSubject.isPaused)
       _actionSubject.sink.add(action);
   }
 
-  Observable<A> map(Observable<A> eventSubject) => eventSubject;
+  /*
+  Used to transform stream.For example you may have SearchAction which performs live search and you need to delay it until
+  user stops entering characters and you want to propagate other actions normally.
 
+  Observable<A> transform (Observable<A> eventSubject)=> Observable.merge([
+   eventSubject.where((ev) => ev is SearchAction).debounce(700).distinct(),
+   eventSubject.where((ev) => !(ev is SearchAction)),
+  ])
+   */
+  Observable<A> transform(Observable<A> eventSubject) => eventSubject;
+
+  /*
+  Initializes BLoC and starts listening for actions.
+   */
   void _listen() {
-    map(_actionSubject)
+    transform(_actionSubject)
         .scan<S>(_processState, initialState)
         .listen(_stateSubject.add);
   }
@@ -42,8 +67,14 @@ abstract class Bloc<A, S> {
     return newState;
   }
 
+  /*
+  Processes actions and map them to adequate state and returns new state.
+   */
   S updateState(S state, A action);
 
+  /*
+  Initial state for BloC
+   */
   S get initialState;
 
   void middlewares(List<Middleware> v) {
--- lib/src/middleware.dart
+++ lib/src/middleware.dart
@@ -1,6 +1,18 @@
 abstract class Middleware<A, S> {
+  /*
+   Next middleware to be called.
+   */
   Middleware next;
 
+  /*
+  Process,transform and map action and state.
+  For example you want to log state to console
+
+  S onState(A action,S state){
+  print(state);
+  return state;
+  }
+   */
   S onState(A action, S state);
 
   S execute(A action, S state) {
